3952709133ea772739a1f969dc2ed5f7bd103ae0
sourav <souravmzdr@gmail.com>
2019-06-26 09:43:00 +0530

    Fixed resolution

--- pong.php
+++ pong.php
@@ -2,18 +2,22 @@
     require 'header.html'
 ?>
     <div class="wrapper">
-        <canvas id="myCanvas" width="580" height="420"></canvas>                
-</div>
-<div class="container_start">
-    <div id="start" onclick="draw();this.style.visibility= 'hidden';">
-       <button>Start</button>
+        <canvas id="myCanvas" width="580" height="420"></canvas>
     </div>
-</div>
-<div class="controls">
-    USE W / S and UP / DOWN ARROW KEYS FOR MOVEMENT.
-</div>
-<script type="text/javascript" src="./js/pong.js"></script>
+    
+    <div class="container_start">
+    <div id="start" onclick="draw();this.style.visibility= 'hidden';">
+    <button type="button" id='start_button'>Start</button>
+    </div>
+    </div>
 
-<?php 
-require 'footer.html'
+    <div class="controls">
+    USE W / S and UP / DOWN ARROW KEYS FOR MOVEMENT.
+    </div>
+
+    <script type="text/javascript" src="./js/pong.js"></script>
+<?php
+    require 'footer.html'
 ?>
+
+   
